2a89c85e86f70b1e324fe74618bb58d851f13a90
mitsram <mitchell.ramos@gmail.com>
2018-06-03 22:00:26 +0800

    Update Chrome.java

--- src/main/java/browser/Chrome.java
+++ src/main/java/browser/Chrome.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
